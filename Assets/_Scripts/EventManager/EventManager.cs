3840dacce80aeddd2840076d2224bac19977e7ec
Nostalex523 <Nostalex523@gmail.com>
2022-09-04 23:05:11 +0300

    Some changes

--- Assets/_Scripts/EventManager/EventManager.cs
+++ Assets/_Scripts/EventManager/EventManager.cs
@@ -8,15 +8,12 @@ public interface IGlobalEvent
 
 public class EventManager : MonoBehaviour
 {
-    private static List<IGlobalEvent> _globalEvents = new List<IGlobalEvent>();
-    private static List<INavMeshSurface> _navMeshSurfaces = new List<INavMeshSurface>();
-
     private static Dictionary<int, HashSet<MonoBehaviour>> _subscribers = new Dictionary<int, HashSet<MonoBehaviour>>();
 
     public static void SubscribeGlobal<T>(MonoBehaviour subscriber) where T : IGlobalEvent
     {
         int typeCode = typeof(T).GetHashCode();
-        
+
         if (_subscribers.TryGetValue(typeCode, out var subscribersList))
         {
             subscribersList.Add(subscriber);
@@ -25,36 +22,31 @@ public class EventManager : MonoBehaviour
         {
             _subscribers.Add(typeCode, new HashSet<MonoBehaviour> {subscriber});
         }
-            
     }
 
     public static void UnsubscribeGlobal<T>(MonoBehaviour subscriber) where T : IGlobalEvent
     {
         int typeCode = typeof(T).GetHashCode();
-        
+
         if (_subscribers.TryGetValue(typeCode, out var subscribersList))
         {
             subscribersList?.Add(subscriber);
         }
-
     }
 
-    public static void RaiseNavMesh<T>(NavMeshSurface surface) where T : IGlobalEvent
+    public static void RaiseNavMesh<T>(NavMeshSurface surface) where T : INavMeshSurface
     {
-        if (typeof(T) == typeof(INavMeshSurface))
+        int typeCode = typeof(T).GetHashCode();
+
+        if (_subscribers.TryGetValue(typeCode, out var subscribersList))
         {
-            int typeCode = typeof(T).GetHashCode();
-        
-            if (_subscribers.TryGetValue(typeCode, out var subscribersList))
+            foreach (var subscriber in subscribersList)
             {
-                foreach (var subscriber in subscribersList)
+                INavMeshSurface navMeshSurface = (INavMeshSurface) subscriber;
+
+                if (navMeshSurface != null)
                 {
-                    INavMeshSurface navMeshSurface = (INavMeshSurface) subscriber;
-                    
-                    if (navMeshSurface != null)
-                    {
-                        navMeshSurface.OnNavMeshCreated(surface);
-                    }
+                    navMeshSurface.OnNavMeshCreated(surface);
                 }
             }
         }
