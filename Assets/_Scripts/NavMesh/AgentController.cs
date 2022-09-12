7a3855dd6f4074830b1f45608750c3615ba032da
Nostalex523 <Nostalex523@gmail.com>
2022-09-12 22:56:06 +0300

    develop 3

--- Assets/_Scripts/NavMesh/AgentController.cs
+++ Assets/_Scripts/NavMesh/AgentController.cs
@@ -14,7 +14,7 @@ public class AgentController : MonoBehaviour
 
     public static int valuess = 123;
 
-    // awake123123123
+    // awake123123123asdasdasd
     private void Awake()
     {
         _mainCamera = Camera.main;
